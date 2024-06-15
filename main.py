71fe4f6d5650947659e0fd066d9fe50ee93477b4
Rémi Canillas <remi.canillas@gmail.com>
2024-06-15 19:53:14 +0200

    add arguments handling -> first working version

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 import json
+import argparse
 
-limit = 10
 def aggregate_transactions(file_path):
     transactions_data = []
+    ## could probably use pandas to do it faster
     with open(file_path,"r") as input_file:
-        i = 0
         for transaction_line in input_file:
             transactions_data.append(json.loads(transaction_line))
-            i += 1
-            if i == limit:
-                break
     return transactions_data
 
 def generate_search_grid(threshold,step=1):
     ## Only work with step size >= 1 (no decimals)
     if threshold:
-        search_grid = [float(threshold)]
+        search_grid = [threshold]
     else: 
         start_value = step
         stop_value = 100
-        #print(start_value,stop_value)
         search_grid = [thresh/100 for thresh in range(start_value,stop_value+step,step)]
-    #print(search_grid)
     return search_grid
 
 def get_predicted_category(threshold, transaction_line):
@@ -29,12 +24,10 @@ def get_predicted_category(threshold, transaction_line):
     predicted_category = None
     for prediction_category, prediction_probability_string in transaction_line["predictions"].items():
         prediction_probability = float(prediction_probability_string)
-        #print(prediction_category, prediction_probability, threshold, max_probability)
         if prediction_probability >= threshold and prediction_probability >= max_probability: 
             ## what if two category have exact probablity ? 
             max_probability = prediction_probability
             predicted_category = prediction_category
-    #print(predicted_category)
     return predicted_category
 
 def rate_guess(true_category, predicted_category):
@@ -42,7 +35,6 @@ def rate_guess(true_category, predicted_category):
         match_result = "GoodGuess"
     else:
         match_result = "BadGuess"
-    #print(true_category, predicted_category, match_result)
     return match_result
 
 def get_match_result(threshold, transaction_line):
@@ -65,11 +57,10 @@ def compute_guess_rate(match_results):
     nb_good_guess = match_results.count("GoodGuess")
     nb_bad_guess = match_results.count("BadGuess")
     penalized_good_guess_rate = (nb_good_guess - 5*nb_bad_guess)/len(match_results)
-    print(nb_good_guess,nb_bad_guess,len(match_results),penalized_good_guess_rate)
     return penalized_good_guess_rate
 
 
-def main(file_path, threshold=None):
+def main(file_path, threshold):
     transaction_data = aggregate_transactions(file_path)
     ## below is brutal grid search, possible improvement -> gradient descent / bayesian optimization
     search_grid = generate_search_grid(threshold)
@@ -84,13 +75,13 @@ def main(file_path, threshold=None):
         penalized_good_guess_rate = compute_guess_rate(match_results)
         if penalized_good_guess_rate > max_penalized_good_guess_rate:
             max_penalized_good_guess_rate = penalized_good_guess_rate
-        print(threshold, match_results, penalized_good_guess_rate)
     print(max_penalized_good_guess_rate)
     return max_penalized_good_guess_rate
 
 
 if __name__ == "__main__":
-    print("hello")
-    #main("predictions.json",0.7)
-    print("___________")
-    main("predictions.json")
+    parser=argparse.ArgumentParser(description="""Un programme qui évalue les performances d'un modèle de données.""")
+    parser.add_argument("dataset", help="le chemin vers le fichier de donnée")
+    parser.add_argument("-t","--threshold",type=float, help="une valeur de seuil.",required=False, default=None)
+    args=parser.parse_args()
+    main(file_path=args.dataset,threshold=args.threshold)
